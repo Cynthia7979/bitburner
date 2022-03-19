b1f1376da2e0b1992f01d9f196015ba3e4d36cea
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-03-19 17:25:55 +0800

    Developing gang script

--- scripts/gangs_my.js
+++ scripts/gangs_my.js
@@ -1,5 +1,4 @@
-/* UNFINISHED! 
-
+/* 
 General idea before I forget: 
 - Hardest tasks for best hackers
 - Always keep wanted level low
@@ -16,28 +15,35 @@ const isNoob = m => avg(m.agi, m.def, m.dex, m.str) > m.hack;
 /** @param {NS} ns **/
 export async function main(ns) {
     ns.tail();
-    
+
+    const args = ns.flags([
+        ['for-rep', false],
+        ['help', false]
+    ]);
+
+    if (args['help']) {
+        ns.tprint('\nUsage:\n\trun gangs_my.js [--for-rep] [--help]\n\t--for-rep\tFarms reputation instead of money\n\t--help\tDisplays this manual.')
+    }
+
+    const forReputation = args['for-rep'];
+
     // Load gang info
     var gangInfo = ns.gang.getGangInformation(),
         gangMembers = ns.gang.getMemberNames(),
         combatMembers = [],
-        hackingMembers = [],
-        trainingMembers = [];
-    
+        hackingMembers = [];
+
     if (!gangInfo.isHacking) {
         ns.tprint('ERROR: This script only works on hacking gangs, now exiting... Sorry!');
         return;
     }
 
-    gangMembers.forEach((member, _, _) => {
-        let memberInfo = ns.gang.getMemberInformation(member);
+    gangMembers.forEach(m => {
+        let memberInfo = ns.gang.getMemberInformation(m);
         if (avg(memberInfo.agi, memberInfo.def, memberInfo.dex, memberInfo.str) > memberInfo.hack) {
-            combatMembers.push(member);
+            combatMembers.push(m);
         } else {
-            hackingMembers.push(member);
-        }
-        if (isNoob(memberInfo)) {
-            trainingMembers.push(member);
+            hackingMembers.push(m);
         }
     })
 
@@ -48,7 +54,26 @@ export async function main(ns) {
             ns.tprint(`Member recruited: ${newName}`);
         }
 
-        
+        gangMembers.forEach(m => {
+            let ascended = false;
+            let ascResult = ns.gang.getAscensionResult(m),
+                currentMult = ns.gang.getMemberInformation(m);
+            if (ascResult) {  // Can ascend
+                if (
+                    (combatMembers.includes(m) &&
+                        currentMult.agi_asc_mult <= 2 * ascResult.agi &&
+                        currentMult.def_asc_mult <= 2 * ascResult.def &&
+                        currentMult.dex_asc_mult <= 2 * ascResult.dex &&
+                        currentMult.str_asc_mult <= 2 * ascResult.str) ||
+                    (hackingMembers.includes(m) &&
+                        currentMult.hack_asc_mult <= 2 * ascResult.hack)) {
+                    ns.gang.ascendMember(m);
+                    ascended = true;
+                }
+            } if (!ascended) {
+                assign(ns, m, hackingMembers.includes(m) ? 'hack' : 'combat', forReputation);
+            }
+        });
 
         await ns.sleep(60000);
     }
@@ -64,4 +89,39 @@ function getName() {  // StackOverflow yehh
             charactersLength));
     }
     return result;
+}
+
+function assign(ns, member, type, forRep = false) {
+    /* Type: "hack" or "combat" */
+    const repJobs = [
+        'DDoS Attacks',
+        'Plant Virus',
+        'Cyberterrorism'
+    ], moneyJobs = [
+        'Ransomware',
+        'Phishing',
+        'Identity Theft',
+        'Fraud & Counterfeiting',
+        'Money Laundering'
+    ],
+        hackHelper = 'Ethical Hacking',
+        combatHelper = 'Vigilante Justice',
+        hackTrain = 'Train Hacking',
+        combatTrain = 'Train Combat',
+        memberInfo = ns.gang.getMemberInformation(member),
+        currentJob = memberInfo.task;;
+    if (isNoob(member)) {}
+}
+
+function tooDifficult(ns, taskName, member) {
+    const taskInfo = ns.gang.getTaskStats(taskName),
+        memberInfo = ns.getMemberInformation(member);
+    let statWeight =
+        (taskInfo.hackWeight / 100) * memberInfo.hack +
+        (taskInfo.strWeight / 100) * memberInfo.str +
+        (taskInfo.defWeight / 100) * memberInfo.def +
+        (taskInfo.dexWeight / 100) * memberInfo.dex +
+        (taskInfo.agiWeight / 100) * memberInfo.agi +
+        (taskInfo.chaWeight / 100) * memberInfo.cha;
+    return statWeight - 4 * task.difficulty <= 0 ? true : false;
 }
